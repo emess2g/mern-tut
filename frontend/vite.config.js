9dbf6d75ff4f528464ea354fe2bfe0015d8cc73e
emess2g <essuonemma84gmail.com>
2024-12-29 21:43:18 +0000

    base url added to func

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,4 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://productstorebackend.vercel.app", //localhost:5000 for local || Replace with your backend server URL if different
-        changeOrigin: true, // Ensures the host header matches the target
-        secure: false, // If your backend uses HTTPS without a valid certificate
-      },
-    },
-  },
 })
